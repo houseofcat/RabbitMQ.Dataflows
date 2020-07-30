957e37bf3c3857dabc41819d991a5ce00ab877cc
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2020-07-30 01:25:33 -0400

    Update Guard.cs

--- src/HouseofCat.Utilities/Errors/Guard.cs
+++ src/HouseofCat.Utilities/Errors/Guard.cs
@@ -11,54 +11,42 @@ namespace HouseofCat.Utilities.Errors
         public static void AgainstNull(object argumentValue, string argumentName)
         {
             if (argumentValue is null)
-            {
                 throw new ArgumentNullException(Strings.Write(Constants.Guard.CantBeNull, argumentName));
-            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AgainstNullOrEmpty(string argumentValue, string argumentName)
         {
             if (string.IsNullOrEmpty(argumentValue))
-            {
                 throw new ArgumentException(Strings.Write(Constants.Guard.CantBeNull, argumentName));
-            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AgainstBothNullOrEmpty(string argumentValue, string argumentName, string secondArgumentValue, string secondArgumentName)
         {
             if (string.IsNullOrEmpty(argumentValue) && string.IsNullOrEmpty(secondArgumentValue))
-            {
                 throw new ArgumentException(Strings.Write(Constants.Guard.BothCantBeNull, argumentName, secondArgumentName));
-            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AgainstNullOrEmpty<T>(IEnumerable<T> argumentValue, string argumentName)
         {
             if (argumentValue?.Any() != true)
-            {
                 throw new ArgumentNullException(Strings.Write(Constants.Guard.CantBeNull, argumentName));
-            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AgainstTrue(bool argumentValue, string argumentName)
         {
             if (argumentValue)
-            {
                 throw new ArgumentException(Strings.Write(Constants.Guard.CantBeTrue, argumentName));
-            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AgainstFalse(bool argumentValue, string argumentName)
         {
             if (argumentValue)
-            {
                 throw new ArgumentException(Strings.Write(Constants.Guard.CantBeFalse, argumentName));
-            }
         }
     }
 }
