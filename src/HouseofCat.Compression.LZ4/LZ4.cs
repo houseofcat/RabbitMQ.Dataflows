999dfbd0b400f04d73a5b24bfb38b6ebbb48d73f
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2020-07-30 01:22:23 -0400

    Update LZ4.cs

--- src/HouseofCat.Compression.LZ4/LZ4.cs
+++ src/HouseofCat.Compression.LZ4/LZ4.cs
@@ -14,20 +14,6 @@ namespace HouseofCat.Compression
 
         private readonly static LZ4DecoderSettings decoderSettings = new LZ4DecoderSettings();
 
-        public static async Task<byte[]> CompressAsync(byte[] input)
-        {
-            using var compressedStream = new MemoryStream();
-
-            using (var lz4Stream = LZ4Stream.Encode(compressedStream, encoderSettings, false))
-            {
-                await lz4Stream
-                    .WriteAsync(input)
-                    .ConfigureAwait(false);
-            }
-
-            return compressedStream.ToArray();
-        }
-
         public static async Task<byte[]> CompressAsync(ReadOnlyMemory<byte> input)
         {
             using var compressedStream = new MemoryStream();
@@ -40,21 +26,6 @@ namespace HouseofCat.Compression
             }
 
             return compressedStream.ToArray();
-        }
-
-        public static async Task<byte[]> DecompressAsync(byte[] input)
-        {
-            using var uncompressedStream = new MemoryStream();
-
-            using (var compressedStream = new MemoryStream(input))
-            using (var lz4Stream = LZ4Stream.Decode(compressedStream, decoderSettings, false))
-            {
-                await lz4Stream
-                    .CopyToAsync(uncompressedStream)
-                    .ConfigureAwait(false);
-            }
-
-            return uncompressedStream.ToArray();
         }
 
         public static async Task<byte[]> DecompressAsync(ReadOnlyMemory<byte> input)
