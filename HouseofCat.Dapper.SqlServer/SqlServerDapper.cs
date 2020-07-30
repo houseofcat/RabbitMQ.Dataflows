90e136541480e919a6dd5b5e79b451fce90513d2
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2020-07-30 00:40:17 -0400

    Update SqlServerDapper.cs

--- HouseofCat.Dapper.SqlServer/SqlServerDapper.cs
+++ HouseofCat.Dapper.SqlServer/SqlServerDapper.cs
@@ -1,9 +1,7 @@
 ﻿using Dapper;
-using FastMember;
 using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Transactions;
 
