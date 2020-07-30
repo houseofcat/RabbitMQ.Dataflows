e3bee9558d3a5276bc0085b610d9628beb04cb73
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2020-07-30 01:13:40 -0400

    Update

--- src/HouseofCat.Reflection/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HouseofCat.Reflection
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/HouseofCat.Reflection/GenericNew.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+
+namespace HouseofCat.Reflection
+{
+    public static class Instance
+    {
+        /// <summary>
+        /// A high performing New generic object instance creator.
+        /// <para>Found this bit of cleverness on StackOverflow while dealing low performance using Generic instantiation.</para>
+        /// <para>https://stackoverflow.com/questions/6582259/fast-creation-of-objects-instead-of-activator-createinstancetype</para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public static class GenericNew<T>
+        {
+            public static readonly Func<T> Instance = Creator();
+
+            private static Func<T> Creator()
+            {
+                Type t = typeof(T);
+                if (t == typeof(string))
+                { return Expression.Lambda<Func<T>>(Expression.Constant(string.Empty)).Compile(); }
+
+                if (t.HasDefaultConstructor())
+                { return Expression.Lambda<Func<T>>(Expression.New(t)).Compile(); }
+
+                return () => (T)FormatterServices.GetUninitializedObject(t);
+            }
+        }
+
+        public static bool HasDefaultConstructor(this Type t)
+        {
+            return t.IsValueType || t.GetConstructor(Type.EmptyTypes) != null;
+        }
+    }
+}
