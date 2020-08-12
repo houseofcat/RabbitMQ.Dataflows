ad3057038c81135517ee0cbe0d919b21ca03871e
Tristan Hyams <thyams@fanatics.com>
2020-08-12 16:51:54 -0400

    Small patch to RabbitMQ.Service.
     * Sacrifice tiny amount of performance on PublishReceipt reading by allowing multiple readers without blocking.

---
+++ src/HouseofCat.Utilities/File/StreamValueReader.cs
@@ -0,0 +1,26 @@
+﻿using HouseofCat.Utilities.Errors;
+using System;
+using System.Collections.Generic;
+
+namespace HouseofCat.Utilities.File
+{
+    public static class Streaming
+    {
+        public static async IAsyncEnumerable<string> StreamOutStringsAsync(string fileNamePath)
+        {
+            Guard.AgainstNull(fileNamePath, nameof(fileNamePath));
+            if (!System.IO.File.Exists(fileNamePath)) throw new ArgumentException($"{fileNamePath} is not a valid file path.");
+
+            using var streamReader = System.IO.File.OpenText(fileNamePath);
+            string currentLine;
+
+            while ((currentLine = await streamReader.ReadLineAsync().ConfigureAwait(false)) != null)
+            {
+                if (!string.IsNullOrWhiteSpace(currentLine))
+                {
+                    yield return currentLine;
+                }
+            }
+        }
+    }
+}
