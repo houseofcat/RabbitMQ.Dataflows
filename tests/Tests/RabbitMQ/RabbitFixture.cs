eaafb8a322c29e511dbb355f0e314c2f3de4a5c9
Peter Hunt <tiggerite@users.noreply.github.com>
2023-03-21 11:52:31 +0000

    RunRabbitMQTests #comment Further simplify rabbit connection check

--- tests/Tests/RabbitMQ/RabbitFixture.cs
+++ tests/Tests/RabbitMQ/RabbitFixture.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
-using DnsClient;
 using HouseofCat.Compression;
 using HouseofCat.Encryption.Providers;
 using HouseofCat.Hashing;
@@ -117,11 +116,8 @@ namespace RabbitMQ
         private static async ValueTask CheckRabbitHostConnection()
         {
             using var cts = new CancellationTokenSource(millisecondsDelay: 1000);
-            var lookupClient = new LookupClient();
-            var queryResponse = await lookupClient.QueryAsync(RabbitHost, QueryType.A, cancellationToken: cts.Token);
-            var addresses = queryResponse.Answers.ARecords().Select(aRecord => aRecord.Address).ToArray();
             using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            await socket.ConnectAsync(addresses, RabbitPort, cts.Token);
+            await socket.ConnectAsync(new DnsEndPoint(RabbitHost, RabbitPort), cts.Token);
         }
 
         private static void UpdateFactoryOptionsWithHost(FactoryOptions factoryOptions)
