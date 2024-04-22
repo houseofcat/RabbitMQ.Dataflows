795c4b7719364eb5c933ce55bc5b38ee50c2afdd
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2024-04-22 11:49:49 -0500

    Update Bytes.cs

--- src/HouseofCat.Utilities/Bytes.cs
+++ src/HouseofCat.Utilities/Bytes.cs
@@ -4,21 +4,21 @@ namespace HouseofCat.Utilities;
 
 public static class Bytes
 {
-    public readonly static byte[] Utf8JsonStartsWith = "{"u8.ToArray();
-    public readonly static byte[] Utf8JsonEndsWith = "}"u8.ToArray();
+    private readonly static byte[] _utf8JsonStartsWith = "{"u8.ToArray();
+    private readonly static byte[] _utf8JsonEndsWith = "}"u8.ToArray();
 
-    public readonly static byte[] Utf8JsonArrayStartsWith = "["u8.ToArray();
-    public readonly static byte[] Utf8JsonArrayEndsWith = "]"u8.ToArray();
+    private readonly static byte[] _utf8JsonArrayStartsWith = "["u8.ToArray();
+    private readonly static byte[] _utf8JsonArrayEndsWith = "]"u8.ToArray();
 
     public static bool IsJson(ReadOnlySpan<byte> data)
     {
-        return data.StartsWith(Utf8JsonStartsWith)
-            && data.EndsWith(Utf8JsonEndsWith);
+        return data.StartsWith(_utf8JsonStartsWith)
+            && data.EndsWith(_utf8JsonEndsWith);
     }
 
     public static bool IsJsonArray(ReadOnlySpan<byte> data)
     {
-        return data.StartsWith(Utf8JsonArrayStartsWith)
-            && data.StartsWith(Utf8JsonArrayEndsWith);
+        return data.StartsWith(_utf8JsonArrayStartsWith)
+            && data.StartsWith(_utf8JsonArrayEndsWith);
     }
 }
